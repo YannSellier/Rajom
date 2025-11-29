3c3eb148c5deca840f0ec0628c9353b19d3ef081
RaptorG-sus <quentin.peschard38@gmail.com>
2025-11-29 04:38:21 +0100

    head set 2 arm

--- Assets/Scripts/Heads/HeadManager.cs
+++ Assets/Scripts/Heads/HeadManager.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 public class HeadManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] 
+    private string inputParam;
     
     [SerializeField]
     private PlayerInput playerInput;
@@ -16,12 +19,14 @@ public class HeadManager : MonoBehaviour
     void Start()
     {
         heads = new List<Head>(GetComponentsInChildren<Head>());
+        setVoid();
+        setHead(heads[3]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInput.actions["interactRobot1"].WasPressedThisFrame())
+        if (playerInput.actions[inputParam].WasPressedThisFrame())
         {
             setHead(heads[1]);
         }
@@ -33,6 +38,14 @@ public class HeadManager : MonoBehaviour
         foreach (Head h in heads)
         {
             h.gameObject.SetActive(h==head);
+        }
+    }
+
+    public void setVoid()
+    {
+        foreach (Head h in heads)
+        {
+            h.gameObject.SetActive(false);
         }
     }
 
